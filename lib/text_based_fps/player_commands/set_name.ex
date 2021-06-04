d4a992a0d28c8331c16f02c067ffaaf6e549206b
Guilherme Sehn <ghsehn@gmail.com>
2021-06-04 19:46:22 -0300

    add validation and success message to 'set-name'

--- lib/text_based_fps/player_commands/set_name.ex
+++ lib/text_based_fps/player_commands/set_name.ex
@@ -1,6 +1,7 @@
 defmodule TextBasedFPS.PlayerCommand.SetName do
   alias TextBasedFPS.PlayerCommand
   alias TextBasedFPS.ServerState
+  import TextBasedFPS.PlayerCommand.Util
 
   @behaviour PlayerCommand
 
@@ -9,7 +10,9 @@ defmodule TextBasedFPS.PlayerCommand.SetName do
     name = String.trim(name)
 
     case validate_name(state, name) do
-      :ok -> {:ok, update_name(state, player, name), nil}
+      :ok ->
+        state = update_name(state, player, name)
+        {:ok, state, success_message(state.players[player.key])}
       {:error, reason} -> {:error, state, reason}
     end
   end
@@ -17,6 +20,7 @@ defmodule TextBasedFPS.PlayerCommand.SetName do
   defp validate_name(state, name) do
     cond do
       String.length(name) > 20 -> {:error, "Name cannot exceed 20 characters"}
+      String.match?(name, ~r/[^a-zA-Z0-9-]/) -> {:error, "Name can only contain letters, numbers and hyphens."}
       name_exists?(state, name) -> {:error, "Name is already in use"}
       true -> :ok
     end
@@ -32,5 +36,12 @@ defmodule TextBasedFPS.PlayerCommand.SetName do
 
   defp name_exists?(state, name) do
     Enum.any?(state.players, fn {_, player} -> player.name == name end)
+  end
+
+  defp success_message(%{name: name, room: nil}) do
+    "Your name is now #{name}. Now, type #{highlight("join-room <room-name>")} to join a room."
+  end
+  defp success_message(%{name: name}) do
+    "Your name is now #{name}."
   end
 end
