f652399bd7e594b4a286642dee3c273d237bef27
Guilherme Sehn <ghsehn@gmail.com>
2021-06-04 23:37:19 -0300

    adjust map path

--- lib/text_based_fps/game_map.ex
+++ lib/text_based_fps/game_map.ex
@@ -1,16 +1,17 @@
 defmodule TextBasedFPS.GameMap do
-  @map_file_path "priv/map.txt"
-  @absolute_map_file_path Path.expand(@map_file_path, File.cwd!)
-
   defstruct [:matrix, :respawn_positions]
 
   def build do
-    text_representation = File.read!(@absolute_map_file_path)
+    text_representation = File.read!(map_file_path())
     TextBasedFPS.GameMap.Builder.build(text_representation)
   end
 
   def update_matrix(game_map, fun) do
     matrix = game_map.matrix
     Map.put(game_map, :matrix, fun.(matrix))
+  end
+
+  defp map_file_path do
+    Path.join(:code.priv_dir(:text_based_fps), "map.txt")
   end
 end
