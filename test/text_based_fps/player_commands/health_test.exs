2589a5e290aa49875953eafb60794f70da70a20b
Guilherme Sehn <ghsehn@gmail.com>
2021-06-06 22:33:07 -0300

    add health command tests

---
+++ test/text_based_fps/player_commands/health_test.exs
@@ -0,0 +1,27 @@
+defmodule TextBasedFPS.PlayerCommands.HealthTest do
+  alias TextBasedFPS.CommandExecutor
+  alias TextBasedFPS.ServerState
+  alias TextBasedFPS.Room
+
+  use ExUnit.Case, async: true
+
+  setup do
+    {_, server_state} = ServerState.new() |> ServerState.add_player("foo")
+    %{state: server_state}
+  end
+
+  test "requires user to be in a room", %{state: state} do
+    assert {:error, _state, error_message} = CommandExecutor.execute(state, "foo", "health")
+    assert error_message =~ "You need to be in a room"
+  end
+
+  test "returns health", %{state: state} do
+    state = state
+    |> ServerState.add_room("spaceship", "foo")
+    |> ServerState.update_room("spaceship", fn room ->
+      Room.update_player(room, "foo", &(Map.put(&1, :health, 50)))
+    end)
+
+    assert {:ok, _state, "Health: 50%"} = CommandExecutor.execute(state, "foo", "health")
+  end
+end
