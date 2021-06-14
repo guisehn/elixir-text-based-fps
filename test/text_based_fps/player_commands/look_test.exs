a6d13668ee64ddbe1fee04ac5b7d9f516fe98e2f
Guilherme Sehn <ghsehn@gmail.com>
2021-06-13 23:21:51 -0300

    fix look test cases

--- test/text_based_fps/player_commands/look_test.exs
+++ test/text_based_fps/player_commands/look_test.exs
@@ -12,8 +12,7 @@ defmodule TextBasedFPS.PlayerCommands.LookTest do
   end
 
   test "requires player to be in a room", %{state: state} do
-    assert {:error, %ServerState{}, error_message} =
-             CommandExecutor.execute(state, "foo", "turn east")
+    assert {:error, %ServerState{}, error_message} = CommandExecutor.execute(state, "foo", "look")
 
     assert error_message =~ "You need to be in a room"
   end
@@ -24,8 +23,7 @@ defmodule TextBasedFPS.PlayerCommands.LookTest do
       |> ServerState.add_room("spaceship", "foo")
       |> ServerState.update_room("spaceship", &Room.kill_player(&1, "foo"))
 
-    assert {:error, %ServerState{}, error_message} =
-             CommandExecutor.execute(state, "foo", "turn east")
+    assert {:error, %ServerState{}, error_message} = CommandExecutor.execute(state, "foo", "look")
 
     assert error_message =~ "You're dead"
   end
