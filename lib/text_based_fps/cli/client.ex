db67171add24b1b12711cc9dfe917962069dd253
Guilherme Sehn <ghsehn@gmail.com>
2021-06-28 08:56:23 -0300

    refactor cli server/client

--- lib/text_based_fps/cli/client.ex
+++ lib/text_based_fps/cli/client.ex
@@ -1,32 +1,62 @@
 defmodule TextBasedFPS.CLI.Client do
-  alias TextBasedFPS.CLI
+  alias TextBasedFPS.{CLI, Text}
 
   def start do
     start_node()
-
-    pid = Node.spawn_link(CLI.Server.node_name(), fn ->
-      receive do
-        pid -> CLI.Server.join_client(pid)
-      end
-    end)
-
-    send pid, self()
-
-    wait_message()
+    server_pid = join_server()
+    Task.start(fn -> ask_command(server_pid) end)
+    wait_server_message()
   end
 
   defp start_node do
-    random = SecureRandom.uuid()
-    node_name = generate_node_name(random)
+    node_name = SecureRandom.uuid() |> generate_node_name()
     Node.start(node_name)
   end
 
-  defp wait_message do
-    receive do
-      {:notification, notification_body} -> IO.puts(notification_body)
+  defp join_server do
+    server_pid =
+      Node.spawn_link(CLI.Server.node_name(), fn ->
+        receive do
+          pid -> CLI.Server.join_client(pid)
+        end
+      end)
+
+    send(server_pid, self())
+
+    server_pid
+  end
+
+  defp ask_command(server_pid) do
+    command = IO.gets(IO.ANSI.reset() <> "> ") |> String.trim()
+
+    if command != "" do
+      send(server_pid, {:command, command})
     end
 
-    wait_message()
+    ask_command(server_pid)
+  end
+
+  defp wait_server_message do
+    receive do
+      message -> handle_server_message(message)
+    end
+
+    wait_server_message()
+  end
+
+  defp handle_server_message({:notification, msg}) do
+    print(msg)
+  end
+
+  defp handle_server_message({:reply, result}) do
+    case result do
+      {:ok, msg} -> if msg != nil, do: print(msg)
+      {:error, msg} -> print(Text.danger(msg))
+    end
+  end
+
+  defp print(msg) do
+    IO.puts(IO.ANSI.clear_line() <> IO.ANSI.cursor_left(999_999) <> IO.ANSI.reset() <> msg)
   end
 
   defp generate_node_name(id) do
--- lib/text_based_fps/cli/server.ex
+++ lib/text_based_fps/cli/server.ex
@@ -1,11 +1,10 @@
 defmodule TextBasedFPS.CLI.Server do
   use Agent
 
-  alias TextBasedFPS.CLI
+  alias TextBasedFPS.{CLI, ServerAgent, Text}
 
   @node_name :"text-based-fps-server@0.0.0.0"
-
-  defdelegate join_client(player_pid), to: TextBasedFPS.CLI.Server.ClientInterface
+  @welcome "Welcome to the text-based FPS! Type #{Text.highlight("set-name <your name>")} to join the game."
 
   def start do
     Node.start(@node_name)
@@ -17,6 +16,31 @@ defmodule TextBasedFPS.CLI.Server do
     IO.puts("")
 
     CLI.Server.Console.start()
+  end
+
+  def join_client(player_pid) do
+    ServerAgent.add_player(player_pid)
+    send(player_pid, {:notification, @welcome})
+    wait_client_message(player_pid)
+  end
+
+  defp wait_client_message(player_pid) do
+    receive do
+      {:command, command} ->
+        result = ServerAgent.run_command(player_pid, command)
+        send(player_pid, {:reply, result})
+        dispatch_notifications()
+    end
+
+    wait_client_message(player_pid)
+  end
+
+  defp dispatch_notifications do
+    Enum.each(ServerAgent.get_and_clear_notifications(), &dispatch_notification/1)
+  end
+
+  defp dispatch_notification(%{body: body, player_key: player_pid}) do
+    send(player_pid, {:notification, body})
   end
 
   def node_name, do: @node_name
